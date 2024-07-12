0c3885520e4c78b16c7f4db4e70402fd0da4ca12
Kelsey Lin <kelseylin2000@gmail.com>
2024-07-12 15:06:26 +0800

    W4H4 Alter assignment-4.sql

--- Week-4/Assignment-4/assignment-4.sql
+++ Week-4/Assignment-4/assignment-4.sql
@@ -1,14 +1,10 @@
+use assignment;
+
 SELECT a.article_id, a.author_id, a.title, a.content, u.email
 FROM article a
 INNER JOIN user u ON a.author_id = u.id;
 
 SELECT *
 FROM article
-ORDER BY  author_id
+ORDER BY  article_id
 LIMIT 6 OFFSET 6;
-
-
-
-
-
-
